e1e0526a84577de56161c0874857f74c15c90020
Tim Qi <i@timqi.com>
2016-04-12 14:25:56 +0800

    first commit

---
+++ src/com/timqi/chapter8/Q1.java
@@ -0,0 +1,17 @@
+package com.timqi.chapter8;
+
+/**
+ * Created by qiqi on 16/4/12.
+ */
+public class Q1 {
+
+  public static boolean isUniqueChar(String s) {
+    if (null == s) return true;
+
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isUniqueChar(null));
+  }
+}
